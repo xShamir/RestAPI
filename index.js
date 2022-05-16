2438792fb1831f5bb6e70c8b69368af509f722be
Shamir Khan <xShamir37@gmail.com>
2022-05-16 07:20:23 +0300

    Made use of .env

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/subscribers', { useNewUrlParser: true });
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 
 db.on('error', (error) => console.error(error));
